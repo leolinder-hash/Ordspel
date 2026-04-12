a915e24bb2823c7e692f6cf36ea70bbb936a0b8a
leolinder-hash <leolinder074@gmail.com>
2026-04-12 17:29:32 +0200

    add start endpoint with session handling for game

--- backend/server/routes/apiRoutes.js
+++ backend/server/routes/apiRoutes.js
@@ -7,4 +7,56 @@ apiRouter.get('/words', (req, res) => {
   res.json(["word1", "word2"]);
 })
 
+const activeSessions = new Map();
+
+apiRouter.post('/game/start', (req, res) => {
+  const { wordLength, allowDuplicateLetters } = req.body;
+  const sessionId = crypto.randomUUID();
+  const randomWord = "apple";
+
+  if (typeof wordLength !== "number") {
+    res.status(400).json({
+      message: "Bad request, length must be a number"
+    })
+
+    return;
+  }
+
+  if (wordLength > 10) {
+    res.status(400).json({
+      message: "Bad request, try a shorter word"
+    })
+
+    return;
+  }
+
+  const gameSession = {
+    gameSessionId: sessionId,
+    createdAt: new Date(),
+
+    settings: {
+      wordLength: wordLength,
+      allowDuplicateLetters: allowDuplicateLetters ?? false,
+    },
+
+    correctWord: randomWord,
+    startedAt: new Date(),
+    endedAt: null,
+
+    status: "active",
+
+    guesses: [],
+
+    resultSubmitted: false
+  };
+
+  activeSessions.set(sessionId, gameSession);
+
+  res.json({
+    sessionId: gameSession.gameSessionId,
+    status: gameSession.status,
+    settings: gameSession.settings
+  });
+})
+
 export default apiRouter;
