60dcd1f8459d7f692cb28450178962d61ecf4d2f
leolinder-hash <leolinder074@gmail.com>
2026-04-13 17:55:10 +0200

    add word-service and parse word list into array

---
+++ backend/services/wordService.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "../data/words.txt");
+const data = fs.readFileSync(filePath, "utf-8");
+
+const words = data.split('\n').
+map(word=> word.trim());
+console.log(words.slice(0,10));
