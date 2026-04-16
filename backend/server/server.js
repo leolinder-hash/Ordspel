1b19858c870367f2ecca58cadc03f3c8e8c2577d
leolinder-hash <leolinder074@gmail.com>
2026-04-16 13:24:43 +0200

    change filename from hbs to handlebars for correct configuration

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,15 +2,24 @@ import express from "express";
 import { engine } from "express-handlebars";
 import pagesRouter from "./routes/pages.js";
 import apiRouter from "./routes/apiRoutes.js"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 5080;
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 
 app.use('/', pagesRouter);
 app.use('/api', apiRouter);
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`server is running on ${port}`)
 })
