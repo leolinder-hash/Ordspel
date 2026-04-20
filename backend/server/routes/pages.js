df688153da32e5703ec0824e36894d0981a2bc6d
leolinder-hash <leolinder074@gmail.com>
2026-04-20 17:56:37 +0200

    add mongoDB filter logic using query params

--- backend/server/routes/pages.js
+++ backend/server/routes/pages.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Highscore from "../models/Highscore.js"
+import { settings } from "cluster";
 
 const pagesRouter = express.Router();
 
@@ -9,8 +10,24 @@ pagesRouter.get('/about', (req, res) => {
 
 //Fetch highscored from DB, sorting them by time and adding ranking for SSR page
 pagesRouter.get('/highscores', async (req, res) => {
+  const {wordLength, allowDuplicateLetters} = req.query;
 
-  const scores = await Highscore.find().sort({ time: 1 }).lean();
+  const parsedWordLength = Number(wordLength);
+  const parsedAllowDuplicates = allowDuplicateLetters === "true";
+
+  const filter = {};
+
+  if (wordLength){
+    filter["settings.wordLength"] = parsedWordLength;
+  }
+
+  if (allowDuplicateLetters !== undefined){
+    filter["settings.allowDuplicateLetters"] = parsedAllowDuplicates;
+  }
+
+  console.log(filter);
+
+  const scores = await Highscore.find(filter).sort({ time: 1 }).lean();
 
   const highscores = scores.map((score, index) => {
     return {
